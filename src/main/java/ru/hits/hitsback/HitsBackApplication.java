d962a6cc97bf0211eb5909b50773c9dcfefe35e3
cyberian <fox4080@gmail.com>
2023-02-13 16:15:41 +0700

    Init Commit

---
+++ src/main/java/ru/hits/hitsback/HitsBackApplication.java
@@ -0,0 +1,13 @@
+package ru.hits.hitsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HitsBackApplication.class, args);
+    }
+
+}
