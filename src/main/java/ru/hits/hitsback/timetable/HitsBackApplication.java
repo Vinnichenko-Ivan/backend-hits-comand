31621a121f634e00414c863237f47d742b4920d6
брамов икита ванович <nabramov19@gmail.com>
2023-02-14 12:44:25 +0700

    new file

---
+++ src/main/java/ru/hits/hitsback/timetable/HitsBackApplication.java
@@ -0,0 +1,13 @@
+package ru.hits.hitsback.timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitsBackApplication.class, args);
+	}
+
+}
