79fa1724ec4d186eb934baec1a997c9681fe7eba
Vinnichenko-Ivan <fox4080@gmail.com>
2023-02-15 17:13:22 +0700

    Сваггер успешно арботает и доступен по ссылке http://localhost:8080/swagger-ui.html

--- src/main/java/ru/hits/hitsback/config/SwaggerConfig.java
+++ src/main/java/ru/hits/hitsback/config/SwaggerConfig.java
@@ -3,19 +3,17 @@ package ru.hits.hitsback.config;
 import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
     @Bean
     public Docket api() {
@@ -25,10 +23,9 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .paths(PathSelectors.any())
                 .build();
     }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("HITS API")
+                .description("My HITS API").build();
     }
+
 }
