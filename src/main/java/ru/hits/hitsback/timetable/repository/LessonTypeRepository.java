75f5c223b141bb54d5cd8fe589a7a65a39465ab6
брамов икита ванович <nabramov19@gmail.com>
2023-02-24 19:55:03 +0700

    Move repo

--- src/main/java/ru/hits/hitsback/timetable/repository/LessonTypeRepository.java
+++ src/main/java/ru/hits/hitsback/timetable/repository/LessonTypeRepository.java
@@ -1,4 +1,4 @@
-package ru.hits.hitsback.repository;
+package ru.hits.hitsback.timetable.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import ru.hits.hitsback.timetable.model.entity.LessonType;
