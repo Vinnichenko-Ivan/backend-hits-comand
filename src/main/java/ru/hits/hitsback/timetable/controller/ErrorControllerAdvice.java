c4e8c0dff92703028dc4d63b63d1353e16a2fb87
feofilovaleksey <Alexfil888@gmail.com>
2023-02-24 19:06:20 +0700

    Task #16926 - добавил систему пробросов ошибок

---
+++ src/main/java/ru/hits/hitsback/timetable/controller/ErrorControllerAdvice.java
@@ -0,0 +1,56 @@
+package ru.hits.hitsback.timetable.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import ru.hits.hitsback.timetable.exception.TeacherNotFoundException;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ErrorControllerAdvice extends ResponseEntityExceptionHandler {
+    private final MessageSource messageSource;
+
+    @Autowired
+    public ErrorControllerAdvice(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", message);
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TeacherNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleTeacherNotFoundException(){
+        return handleCustomException(HttpStatus.NOT_FOUND, "teacher.not-found");
+    }
+
+    private String getErrorMessage(String code){
+        return messageSource.getMessage(code, null, Locale.ENGLISH);
+    }
+
+    private ResponseEntity<Map<String, Object>> handleCustomException(HttpStatus status, String code) {
+        Map<String, Object> message = new HashMap<>();
+        message.put("message", getErrorMessage(code));
+        return ResponseEntity.status(status).body(message);
+    }
+}
