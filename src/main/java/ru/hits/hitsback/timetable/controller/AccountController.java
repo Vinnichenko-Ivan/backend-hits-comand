9e1574225c03759ebeff3fb814bd9ccfad5baacc
feofilovaleksey <Alexfil888@gmail.com>
2023-02-25 23:36:28 +0700

    Task #16925 - убрал из запросов Authentication параметр

--- src/main/java/ru/hits/hitsback/timetable/controller/AccountController.java
+++ src/main/java/ru/hits/hitsback/timetable/controller/AccountController.java
@@ -1,15 +1,13 @@
 package ru.hits.hitsback.timetable.controller;
 
 import jakarta.validation.Valid;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.hits.hitsback.timetable.configuration.UrlConstant;
 import ru.hits.hitsback.timetable.dto.acoount.AccountDto;
-import ru.hits.hitsback.timetable.dto.group.GroupIdDto;
 import ru.hits.hitsback.timetable.dto.acoount.PasswordModifyDto;
+import ru.hits.hitsback.timetable.dto.group.GroupIdDto;
 
 import java.util.List;
 
@@ -20,7 +18,7 @@ import static ru.hits.hitsback.timetable.configuration.UrlConstant.PROFILE_URL;
 @RequestMapping(value = BASE_URL + PROFILE_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class AccountController {
     @GetMapping("me")
-    public ResponseEntity<AccountDto> fetchAccountInfo(Authentication authentication){
+    public ResponseEntity<AccountDto> fetchAccountInfo(/*Authentication authentication*/){
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
     }
 
@@ -35,7 +33,7 @@ public class AccountController {
     }
 
     @PutMapping(value = "security/password", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> changePassword(@Valid @RequestBody PasswordModifyDto passwordModifyDto, Authentication authentication){
+    public ResponseEntity<Void> changePassword(@Valid @RequestBody PasswordModifyDto passwordModifyDto/*, Authentication authentication*/){
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
     }
 }
--- src/main/java/ru/hits/hitsback/timetable/controller/ScheduleController.java
+++ src/main/java/ru/hits/hitsback/timetable/controller/ScheduleController.java
@@ -1,6 +1,5 @@
 package ru.hits.hitsback.timetable.controller;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,8 +21,8 @@ public class ScheduleController {
     @GetMapping
     public ResponseEntity<List<DayScheduleDto>> fetchSchedule(
             @RequestParam Date startDate,
-            @RequestParam(required = false) Date endDate,
-            Authentication authentication
+            @RequestParam(required = false) Date endDate/*,
+            Authentication authentication*/
     ) {
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
     }
@@ -37,6 +36,7 @@ public class ScheduleController {
         GroupIdDto groupIdDto = new GroupIdDto(id);
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
     }
+
     @GetMapping(value = "teacher/{id}")
     public ResponseEntity<List<DayScheduleDto>> fetchTeacherSchedule(
             @RequestParam Date startDate,
