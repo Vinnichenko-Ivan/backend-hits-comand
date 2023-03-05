17883c812634ef1189727cdad6c0d0da7b701d76
Vinnichenko-Ivan <fox4080@gmail.com>
2023-03-05 10:04:39 +0700

    Настройка безопастности. Не нашел фильтры по типу запроса, так что дополнительно проверяйте роль в запросе.

--- src/main/java/ru/hits/hitsback/timetable/configuration/WebSecurityConfig.java
+++ src/main/java/ru/hits/hitsback/timetable/configuration/WebSecurityConfig.java
@@ -32,6 +32,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf().disable()
+                .cors().disable()
                 .formLogin().disable()
                 .httpBasic().disable()
                 .securityContext()
@@ -40,14 +41,50 @@ public class WebSecurityConfig {
                 .requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/webjars/**")).permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/v3/api-docs/**")).permitAll()
+
                 .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + AUTHORISATION_URL + "/sign-out")).authenticated()
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + AUTHORISATION_URL + "/sign-out-all")).authenticated()
                 .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + AUTHORISATION_URL + "/sign-in")).permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + AUTHORISATION_URL + "/teacher/sign-up")).permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + AUTHORISATION_URL + "/student/sign-up")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + REQUEST_URL + "/**")).hasAuthority("Admin")
-                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll() //TODO для дебага пока так
-                .requestMatchers(BASE_URL + AUTHORISATION_URL + "/sign-out").authenticated()
-                .requestMatchers("/api/v1/teacher").authenticated()
+
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + PROFILE_URL)).authenticated()
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + PROFILE_URL + "/me")).hasAuthority("Admin")
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + PROFILE_URL + "/group")).hasAuthority("Student")
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + PROFILE_URL + "/security/password")).authenticated()
+
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + GROUP_URL)).authenticated()
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + GROUP_URL + "/**")).hasAuthority("Admin")
+
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + STUDY_ROOM_URL)).authenticated()
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + STUDY_ROOM_URL + "/**")).hasAuthority("Admin")
+
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + TEACHER_URL)).authenticated()
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + TEACHER_URL + "/**")).hasAuthority("Admin")
+
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + SUBJECT_URL)).authenticated()
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + SUBJECT_URL + "/**")).hasAuthority("Admin")
+
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + SCHEDULE_URL)).authenticated()
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + SCHEDULE_URL + "/group/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + SCHEDULE_URL + "/teacher/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + SCHEDULE_URL + "/staff")).hasAuthority("ScheduleWriter")
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + SCHEDULE_URL + "/lesson-time")).hasAuthority("ScheduleWriter")
+
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + LESSON_URL + "/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + LESSON_URL + "/type")).hasAuthority("ScheduleWriter")
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + LESSON_URL + "/staff")).hasAuthority("ScheduleWriter")
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + LESSON_URL + "/lesson-group")).hasAuthority("ScheduleWriter")
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + LESSON_URL + "/staff")).hasAuthority("ScheduleWriter")
+
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + REQUEST_URL + "/registration")).hasAuthority("Admin")
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + REQUEST_URL + "/group")).hasAuthority("Admin")
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + REQUEST_URL + "/registration/**")).hasAuthority("Admin")
+                .requestMatchers(new AntPathRequestMatcher("/" + BASE_URL + REQUEST_URL + "/group/**")).hasAuthority("Admin")
+
+
+//                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll() //TODO для дебага пока так
+
                 .and()
                 .addFilterAfter(jwtFilter, SecurityContextHolderFilter.class)
                 .build();
