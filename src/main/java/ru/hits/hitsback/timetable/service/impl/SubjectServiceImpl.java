1876ca88c2c1771a33701bc7bd40931874473a49
Vinnichenko-Ivan <fox4080@gmail.com>
2023-03-06 08:34:59 +0700

    Фикс времени + рефакторинг

---
+++ src/main/java/ru/hits/hitsback/timetable/service/impl/SubjectServiceImpl.java
@@ -0,0 +1,59 @@
+package ru.hits.hitsback.timetable.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.hits.hitsback.timetable.exception.SubjectCopyException;
+import ru.hits.hitsback.timetable.exception.SubjectIsAlreadyExistException;
+import ru.hits.hitsback.timetable.mapper.SubjectMapper;
+import ru.hits.hitsback.timetable.model.dto.subject.SubjectCreateDto;
+import ru.hits.hitsback.timetable.model.dto.subject.SubjectDto;
+import ru.hits.hitsback.timetable.model.dto.subject.SubjectIdDto;
+import ru.hits.hitsback.timetable.model.entity.Subject;
+import ru.hits.hitsback.timetable.repository.SubjectRepository;
+import ru.hits.hitsback.timetable.service.SubjectService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class SubjectServiceImpl implements SubjectService {
+
+    private final SubjectMapper subjectMapper;
+
+    private final SubjectRepository subjectRepository;
+
+    public List<SubjectDto> fetchSubjects() {
+        return subjectRepository.findAll().stream().map(subjectMapper::map).collect(Collectors.toList());
+    }
+
+    public SubjectIdDto createSubject(SubjectCreateDto subjectCreateDto) {
+        if (subjectRepository.existsByName(subjectCreateDto.getName())) {
+            throw new SubjectCopyException();
+        } else {
+            subjectRepository.save(subjectMapper.map(subjectCreateDto));
+        }
+        return new SubjectIdDto(subjectRepository.getSubjectByName(subjectCreateDto.getName()).getId().toString());
+    }
+
+    public void modifySubject(SubjectDto subjectDto) {
+        Subject subject = subjectRepository.findById(subjectDto.getSubjectIdDto().getId()).orElse(null);
+        if (subject == null) {
+            throw new SubjectIsAlreadyExistException();
+        } else if (subjectRepository.existsByName(subjectDto.getName())) {
+            throw new SubjectCopyException();
+        } else {
+            subject.setName(subjectDto.getName());
+            subjectRepository.save(subject);
+        }
+    }
+
+    public void deleteSubject(SubjectIdDto subjectIdDto) {
+        Subject subject = subjectRepository.findById(subjectIdDto.getId()).orElse(null);
+        if (subject == null) {
+            throw new SubjectIsAlreadyExistException();
+        } else {
+            subjectRepository.delete(subject);
+        }
+    }
+}
