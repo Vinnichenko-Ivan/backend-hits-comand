17de44cfd7a0950a7473bdaf53ecd869f4e71aad
feofilovaleksey <Alexfil888@gmail.com>
2023-03-09 01:06:51 +0700

    Task #17335 - закончил логику ScheduleService

--- src/main/java/ru/hits/hitsback/timetable/service/schedule/ScheduleService.java
+++ src/main/java/ru/hits/hitsback/timetable/service/schedule/ScheduleService.java
@@ -6,11 +6,12 @@ import ru.hits.hitsback.timetable.model.dto.schedule.LessonOptionsDto;
 import ru.hits.hitsback.timetable.model.dto.schedule.LessonTimeDto;
 import ru.hits.hitsback.timetable.model.dto.schedule.TimeIntervalDto;
 import ru.hits.hitsback.timetable.model.dto.teacher.TeacherIdDto;
+import ru.hits.hitsback.timetable.model.entity.Account;
 
 import java.util.List;
 
 public interface ScheduleService {
-    List<DayScheduleDto> fetchSchedule(TimeIntervalDto timeIntervalDto);
+    List<DayScheduleDto> fetchSchedule(TimeIntervalDto timeIntervalDto, Account account);
 
     List<DayScheduleDto> fetchGroupSchedule(TimeIntervalDto timeIntervalDto, GroupIdDto groupIdDto);
 
